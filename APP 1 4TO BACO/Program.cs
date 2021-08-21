a51dc5b1b0c8103a10463aadbc11f345dc71b6a8
Jcarlosescobar <85755807+Jcarlosescobar@users.noreply.github.com>
2021-08-20 18:08:24 -0600

    Add files via upload

---
+++ APP 1 4TO BACO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APP_1_4TO_BACO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
